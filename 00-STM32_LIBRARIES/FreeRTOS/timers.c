3032371ec56d0ae97e02d626c179231aee0bf5bc
Tilen Majerle <tilen.majerle@gmail.com>
2017-02-10 08:30:49 +0100

    Updated RTOS timers

--- 00-STM32_LIBRARIES/FreeRTOS/timers.c
+++ 00-STM32_LIBRARIES/FreeRTOS/timers.c
@@ -100,20 +100,6 @@ configUSE_TIMERS is set to 1 in FreeRTOSConfig.h. */
 /* Misc definitions. */
 #define tmrNO_DELAY		( TickType_t ) 0U
 
-/* The definition of the timers themselves. */
-typedef struct tmrTimerControl
-{
-	const char				*pcTimerName;		/*<< Text name.  This is not used by the kernel, it is included simply to make debugging easier. */ /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
-	ListItem_t				xTimerListItem;		/*<< Standard linked list item as used by all kernel features for event management. */
-	TickType_t				xTimerPeriodInTicks;/*<< How quickly and often the timer expires. */
-	UBaseType_t				uxAutoReload;		/*<< Set to pdTRUE if the timer should be automatically restarted once expired.  Set to pdFALSE if the timer is, in effect, a one-shot timer. */
-	void 					*pvTimerID;			/*<< An ID to identify the timer.  This allows the timer to be identified when the same callback is used for multiple timers. */
-	TimerCallbackFunction_t	pxCallbackFunction;	/*<< The function that will be called when the timer expires. */
-	#if( configUSE_TRACE_FACILITY == 1 )
-		UBaseType_t			uxTimerNumber;		/*<< An ID assigned by trace tools such as FreeRTOS+Trace */
-	#endif
-} xTIMER;
-
 /* The old xTIMER name is maintained above then typedefed to the new Timer_t
 name below to enable the use of older kernel aware debuggers. */
 typedef xTIMER Timer_t;
